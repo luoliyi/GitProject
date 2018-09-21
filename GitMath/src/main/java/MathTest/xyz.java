e19ba7f32c4051a640b0528595dd0c834209d18a
luoliyi <mopln510@qq.com>
2018-09-21 08:39:47 +0800

    写的xyz类，补。

---
+++ GitMath/src/main/java/MathTest/xyz.java
@@ -0,0 +1,12 @@
+package MathTest;
+
+public class xyz {
+
+    public static int xyz(int x, int y, int z){
+        return x+y+z;
+    }
+
+    public static void main(String[]args){
+        System.out.println(xyz(4,62,2));
+    }
+}
